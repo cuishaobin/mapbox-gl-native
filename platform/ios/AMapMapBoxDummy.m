daf22937f38f8e1f464f3af7c07d5f112fb5d8ac
shaobin.csb <shaobin.csb@alibaba-inc.com>
2019-04-30 19:02:17 +0800

    去掉不需要的iOS层文件，减少包大小

---
+++ platform/ios/AMapMapBoxDummy.m
@@ -0,0 +1,13 @@
+//
+//  AMapMapBoxDummy.m
+//  static
+//
+//  Created by shaobin on 2018/8/21.
+//  Copyright © 2018年 Mapbox. All rights reserved.
+//
+
+#import "AMapMapBoxDummy.h"
+
+@implementation AMapMapBoxDummy
+
+@end
