3f3492389d73244531d17cb9af7c4e15381ed5f1
shaobin.csb <shaobin.csb@alibaba-inc.com>
2020-03-17 15:30:46 +0800

    体积裁剪：去掉不需要的功能模块

--- include/mbgl/util/mbgl-coreConfig.h
+++ include/mbgl/util/mbgl-coreConfig.h
@@ -9,28 +9,28 @@
 #define mbgl_coreConfig_h
 
 #ifndef mbgl_core_include_heatmaplayer
-#define mbgl_core_include_heatmaplayer 1
+#define mbgl_core_include_heatmaplayer 0
 #endif
 
 /*****************************************************************
  mbgl_core_include_rasterlayer 对 hillshade有结构体依赖
  *****************************************************************/
 #ifndef mbgl_core_include_hillshadelayer
-#define mbgl_core_include_hillshadelayer 1
+#define mbgl_core_include_hillshadelayer 0
 #endif
 
 #ifndef mbgl_core_include_rasterlayer
-#define mbgl_core_include_rasterlayer 1
+#define mbgl_core_include_rasterlayer 0
 #endif
 /*****************************************************************
  *****************************************************************/
 
 #ifndef mbgl_core_include_circlelayer
-#define mbgl_core_include_circlelayer 1
+#define mbgl_core_include_circlelayer 0
 #endif
 
 #ifndef mbgl_core_include_geojson
-#define mbgl_core_include_geojson 1
+#define mbgl_core_include_geojson 0
 #endif
 
 #endif /* mbgl_coreConfig_h */
